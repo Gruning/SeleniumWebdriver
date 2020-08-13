e91e5635086c38174f21a87095c8d8238485e841
Alberto Gruning <alberto.gruning@ndvsistemas.com>
2020-08-12 22:13:24 -0300

    using selenium chrome webddriver

--- SeleniumWebdriver/UnitTest1.cs
+++ SeleniumWebdriver/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
 
 namespace SeleniumWebdriver
 {
@@ -9,6 +10,7 @@ namespace SeleniumWebdriver
         [TestMethod]
         public void TestMethod1()
         {
+            ChromeDriver
         }
     }
 }
