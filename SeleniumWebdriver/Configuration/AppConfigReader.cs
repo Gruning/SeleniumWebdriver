77eae6cd2205fa652a46c16657ca1056cc460ff3
Alberto Gruning <alberto.gruning@ndvsistemas.com>
2020-09-01 19:53:55 -0300

    implementing Iconfig

--- SeleniumWebdriver/Configuration/AppConfigReader.cs
+++ SeleniumWebdriver/Configuration/AppConfigReader.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SeleniumWebdriver.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,21 @@ using System.Threading.Tasks;
 
 namespace SeleniumWebdriver.Configuration
 {
-    public class AppConfigReader
+    public class AppConfigReader : IConfig
     {
+        public BrowserType GetBrowser()
+        {
+            throw new NotImplementedException();
+        }
 
+        public string GetPassword()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetUsername()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
