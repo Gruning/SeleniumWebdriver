79a61bd8d6be03978afe9a4192fb6dfb3e323da7
Alberto Gruning <alberto.gruning@ndvsistemas.com>
2020-09-07 21:13:11 -0300

    39SettingUpBaseClass

---
+++ SeleniumWebdriver/BaseClasses/BaseClass.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumWebdriver.BaseClasses
+{
+    public class BaseClass
+    {
+        private static IWebDriver GetChromeDriver()
+        {
+
+        }
+    }
+}
