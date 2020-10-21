e7f5047bf961ca1a3c3f1a6e4e4aa8057086c7e5
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-10-20 21:38:50 -0300

    xmlReader

--- SeleniumWebdriver/UnitTest1.cs
+++ SeleniumWebdriver/UnitTest1.cs
@@ -4,6 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using SeleniumWebdriver.Configuration;
+using SeleniumWebdriver.Interfaces;
 
 namespace SeleniumWebdriver
 {
@@ -26,6 +27,15 @@ namespace SeleniumWebdriver
             Console.WriteLine(ConfigurationManager.AppSettings.Get("Username"));
             Console.WriteLine(ConfigurationManager.AppSettings.Get("Password"));
             Console.WriteLine((int)BrowserType.Chrome);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            IConfig config = new XmlReader();
+            Console.WriteLine($"Browser {config.GetBrowser()}");
+            Console.WriteLine($"Password {config.GetPassword()}");
+            Console.WriteLine($"Username {config.GetUsername()}");
+
         }
     }
 }
