c956817848f19c4489e56a874f3f51a3a0b2fb99
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-10-23 18:59:27 -0300

    cap41-pageNavigation

---
+++ SeleniumWebdriver/TestScript/PageNavigation/TestPageNavigation.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using SeleniumWebdriver.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumWebdriver.TestScript.PageNavigation
+{
+    [TestClass]
+    public class TestPageNavigation
+    {
+        [TestMethod]
+        public void OpenPage()
+        {
+            INavigation page = ObjectRepository.Driver.Navigate();
+            page.GoToUrl(ObjectRepository.Config.GetWebSite());
+        }
+    }
+}
