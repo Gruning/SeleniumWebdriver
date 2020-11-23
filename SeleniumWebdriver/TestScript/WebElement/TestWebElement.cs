324e78374765f5e54e62488d0ad423c703762b63
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-11-22 21:13:19 -0300

    cap44-Elements

---
+++ SeleniumWebdriver/TestScript/WebElement/TestWebElement.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using SeleniumWebdriver.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumWebdriver.TestScript.WebElement
+{
+    [TestClass]
+    public class TestWebElement
+    {
+        [TestMethod]public void GetElement() {
+            ObjectRepository.Driver.FindElement(By.TagName("input"));
+            ObjectRepository.Driver.FindElement(By.ClassName("btn"));
+            ObjectRepository.Driver.FindElement(By.CssSelector("#find"));
+            ObjectRepository.Driver.FindElement(By.LinkText("Gmail"));
+            ObjectRepository.Driver.FindElement(By.PartialLinkText("Gmail"));
+            ObjectRepository.Driver.FindElement(By.Name("Gmail"));
+            ObjectRepository.Driver.FindElement(By.Id("footcnt"));
+            ObjectRepository.Driver.FindElement(By.XPath("Gmail"));
+
+        }
+    }
+}
