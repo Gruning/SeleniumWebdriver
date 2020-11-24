0dd73e35ea78e932c5d9ee155867882571e3f713
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-11-24 18:40:50 -0300

    cap45-indetifyingWebElem

--- SeleniumWebdriver/TestScript/WebElement/TEstWebElement.cs
+++ SeleniumWebdriver/TestScript/WebElement/TEstWebElement.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
+using SeleniumWebdriver.ComponentHelper;
 using SeleniumWebdriver.Settings;
 using System;
 using System.Collections.Generic;
@@ -13,14 +14,24 @@ namespace SeleniumWebdriver.TestScript.WebElement
     public class TestWebElement
     {
         [TestMethod]public void GetElement() {
-            ObjectRepository.Driver.FindElement(By.TagName("input"));
-            ObjectRepository.Driver.FindElement(By.ClassName("btn"));
-            ObjectRepository.Driver.FindElement(By.CssSelector("#find"));
-            ObjectRepository.Driver.FindElement(By.LinkText("Gmail"));
-            ObjectRepository.Driver.FindElement(By.PartialLinkText("Gmail"));
-            ObjectRepository.Driver.FindElement(By.Name("Gmail"));
-            ObjectRepository.Driver.FindElement(By.Id("footcnt"));
-            ObjectRepository.Driver.FindElement(By.XPath("Gmail"));
+            NavigationHelper.NavigateToUrl(ObjectRepository.Config.GetWebSite());
+            try
+            {
+
+                ObjectRepository.Driver.FindElement(By.TagName("input"));
+                ObjectRepository.Driver.FindElement(By.ClassName("btn"));
+                ObjectRepository.Driver.FindElement(By.CssSelector("#find"));
+                ObjectRepository.Driver.FindElement(By.LinkText("Gmail"));
+                //ObjectRepository.Driver.FindElement(By.PartialLinkText("Gmail"));
+                //ObjectRepository.Driver.FindElement(By.Name("Gmail"));
+                ObjectRepository.Driver.FindElement(By.Id("footcnt"));
+                //ObjectRepository.Driver.FindElement(By.XPath("Gmail"));
+            }
+            catch (NoSuchElementException e)
+            {
+
+                Console.WriteLine(e);
+            }
 
         }
     }
