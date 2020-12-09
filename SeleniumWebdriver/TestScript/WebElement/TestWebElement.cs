a5ecd45c33d853c1d44029bcb94ce609ca6a22ac
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-12-09 19:05:54 -0300

    find imput

--- SeleniumWebdriver/TestScript/WebElement/TestWebElement.cs
+++ SeleniumWebdriver/TestScript/WebElement/TestWebElement.cs
@@ -17,20 +17,28 @@ namespace SeleniumWebdriver.TestScript.WebElement
             NavigationHelper.NavigateToUrl(ObjectRepository.Config.GetWebSite());
             try
             {
+                ObjectRepository.Driver.FindElements(By.TagName("input"));
 
-                var byTag = ObjectRepository.Driver.FindElement(By.TagName("input"));
-                var byCss = ObjectRepository.Driver.FindElement(By.CssSelector("#find"));
-                var byId = ObjectRepository.Driver.FindElement(By.Id("footcnt"));
-                var byLink = ObjectRepository.Driver.FindElement(By.LinkText("Gmail"));
-                var byBlass = ObjectRepository.Driver.FindElement(By.ClassName("btn"));
+                // var byCss = ObjectRepository.Driver.FindElement(By.CssSelector("#find"));
+                //var byBlass = ObjectRepository.Driver.FindElement(By.ClassName("btn"));
                 //ObjectRepository.Driver.FindElement(By.PartialLinkText("Gmail"));
                 //ObjectRepository.Driver.FindElement(By.Name("Gmail"));
                 //ObjectRepository.Driver.FindElement(By.XPath("Gmail"));
-                IList<string> list = new List<string>();
-                list.Add("Task 1");
-                list.Add("Task 2");
-                list.Add("Task 3");
-                Console.WriteLine($"size {list.Count}");
+
+                //var byTag = ObjectRepository.Driver.FindElement(By.TagName("input"));
+                //var byId = ObjectRepository.Driver.FindElement(By.Id("footcnt"));
+                //var byLink = ObjectRepository.Driver.FindElement(By.LinkText("Gmail"));
+                //IList<string> list = new List<string>();
+                //list.Add("Task 1");
+                //list.Add("Task 2");
+                //list.Add("Task 3");
+                //Console.WriteLine($"size {list.Count}");
+                //list.Remove("Task 2");
+                //Console.WriteLine($"size {list.Count}");
+                //Console.WriteLine($"Value {list[0]}");
+                //list.Clear();
+                //Console.WriteLine($"size {list.Count}");
+
 
             }
             catch (NoSuchElementException e)
