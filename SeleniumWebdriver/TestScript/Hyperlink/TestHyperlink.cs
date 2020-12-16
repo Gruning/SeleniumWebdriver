bacf3246dc5e9dea5a932e9d238a70d3ba182dd9
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-12-16 18:40:21 -0300

    cap45-HandlinElements

---
+++ SeleniumWebdriver/TestScript/Hyperlink/TestHyperlink.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using SeleniumWebdriver.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumWebdriver.TestScript.Hyperlink
+{
+    [TestClass]
+    class TestHyperlink
+    {
+        public void clickLink() { 
+             ObjectRepository.Driver.FindElement(By.LinkText("https://www.google.com.ar/imghp?hl=es-419&amp;tab=wi&amp;ogbl"))
+        }
+    }
+}
