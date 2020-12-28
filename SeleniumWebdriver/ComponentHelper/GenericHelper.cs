ccfce30eb0f1032dd764b87c38154b3c609ea37f
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-12-28 18:32:00 -0300

    cap45-genericHelper

---
+++ SeleniumWebdriver/ComponentHelper/GenericHelper.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using SeleniumWebdriver.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumWebdriver.ComponentHelper
+{
+    public class GenericHelper
+    {
+        public static bool isElementPresent(By Locator) {
+            try{
+                return ObjectRepository.Driver.FindElements(Locator).Count == 1;
+            } catch (Exception) {
+                return false;
+            }
+        }
+        public static IWebElement GetElement(By Locator)
+        {
+            if (isElementPresent(Locator))
+                return ObjectRepository.Driver.FindElement(Locator);
+            else
+                throw new NoSuchElementException($"Element not found:{Locator.ToString()}");
+        }
+    }
+}
