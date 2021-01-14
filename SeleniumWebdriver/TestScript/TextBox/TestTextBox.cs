e11855f0f5a36dabd0aefefca2db31a19525646b
AlbertoGruning <alberto.gruning.zen@gmail.com>
2021-01-14 18:45:04 -0300

    cap45-textBox

---
+++ SeleniumWebdriver/TestScript/TextBox/TestTextBox.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumWebdriver.TestScript.TextBox
+{
+    [TestClass]
+    public class TestTextBox
+    {
+        [TestMethod]
+        public void TextBox() { 
+        }
+    }
+}
